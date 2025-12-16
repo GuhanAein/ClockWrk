9371b7954ae678c92ca0d07cadd44f259ae759ba
Guhan <guhannadin@Guhans-MacBook.local>
2025-12-16 18:31:00 +0530

    added backend

---
+++ backend/src/test/java/com/task/clockwrk/clockWork/ClockWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.clockwrk.clockWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
