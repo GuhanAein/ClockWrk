3f8352996dfc4724b7270c7910385a240a3beaa4
Guhan Aein <102289063+GuhanAein@users.noreply.github.com>
2026-01-05 23:45:19 +0530

    Update production environment URLs

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    baseUrl: 'http://44.195.2.208:8080',
-    apiUrl: 'http://44.195.2.208:8080/api'
+  production: true,
+  baseUrl: 'https://api.clockwrk.in',
+  apiUrl: 'https://api.clockwrk.in/api'
 };
